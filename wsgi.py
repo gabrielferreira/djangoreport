cffe4066de3180c9f46a1ddb473866a004405201
Gabriel Ferreira <gabrielferreira@users.noreply.github.com>
2013-08-29 17:24:14 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -13,13 +13,23 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
+
 import os
+
+import sys
+
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, 'djangoreport'))
+sys.path.insert(0, root_path)
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "djangoreport.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+# import os
+
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
